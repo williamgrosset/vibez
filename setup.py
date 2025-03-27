259f141b6ef0adad384ceacf621147b86774cea1
William Grosset <williamhgrosset@gmail.com>
2025-03-27 09:35:49 -0700

    Update project metadata

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="vibez",
     version="0.1.0",
+    description="Lightweight CLI tool to extract file contents for LLMs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/williamgrosset/vibez",
+    project_urls={
+        "Source": "https://github.com/williamgrosset/vibez",
+        "Bug Tracker": "https://github.com/williamgrosset/vibez/issues",
+        "Documentation": "https://github.com/williamgrosset/vibez#readme",
+    },
     packages=find_packages(),
-    install_requires=[
-        "pyperclip"
-    ],
+    install_requires=["pyperclip"],
     entry_points={
         "console_scripts": [
             "vibez=vibez.main:main"
@@ -15,7 +25,13 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Utilities",
     ],
-    python_requires=">=3.6"
+    python_requires=">=3.6",
+    license="MIT",
+    include_package_data=True,
 )
